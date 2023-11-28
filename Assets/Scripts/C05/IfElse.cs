1eda75fa609558f6bed346be6e68f93941193035
SBCskkuk <tjdnftkadbr@skkukdp.re.kr>
2023-11-28 15:00:42 +0900

    5장 초반
    초반

--- Assets/Scripts/C05/IfElse.cs
+++ Assets/Scripts/C05/IfElse.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,29 @@ public class IfElse : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        string input = "3";
+        int num = Int32.Parse(input);
 
-    // Update is called once per frame
-    void Update()
-    {
+        if (num < 0 )
+        {
+            Debug.Log("擠熱");
+        }
+        else if (num > 0)
+        {
+            Debug.Log("曄熱");
+        }
+        else
+        {
+            Debug.Log("0");
+        }
+        if (num % 2 == 0)
+        {
+            Debug.Log("礎熱");
+        }
+        else
+        {
+            Debug.Log("�汝�");
+        }
         
     }
 }
